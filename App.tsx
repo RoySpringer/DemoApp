35e75c35ea90e62a77b2c9b77617c9cf8e5f07d8
Roy Springer <rspringer@slip-app.nl>
2024-05-15 15:13:56 +0200

    chore: added minimal demo code

--- App.tsx
+++ App.tsx
@@ -1,117 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useCallback, useEffect, useState} from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Dimensions, FlatList, Image, StyleSheet, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+const windowSize = 3;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const image1 = require('./assets/image1.jpg');
+const image2 = require('./assets/image2.jpg');
+const image3 = require('./assets/image3.jpg');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const images = [
+  {id: '1', image: image1},
+  {id: '2', image: image2},
+  {id: '3', image: image3},
+];
+
+const keyExtractor = (item: {id: string}): string => item.id;
+
+const getItemLayout = (_data: any, index: number) => ({
+  length: width,
+  offset: width * index,
+  index,
+});
+
+function App(): React.JSX.Element {
+  const listRef = React.useRef<FlatList>(null);
+  const [currentIndex, setCurrentSegmentIndex] = useState<number>(0);
+
+  const handleViewableItemsChanged = useCallback((event: any) => {
+    console.log('ViewableItems', event.viewableItems);
+    const singleViewableItem = event.viewableItems.length === 1;
+
+    // NOTE: We only care about one item taking whole viewport (list not  scrolled in between 2 items)
+    if (!singleViewableItem) {
+      return;
+    }
+
+    const currentViewableItem = event.viewableItems.shift();
+
+    if (!currentViewableItem) {
+      return;
+    }
+
+    const {index: currentViewableItemIndex} = currentViewableItem;
+
+    console.log('currentViewableItemIndex', currentViewableItemIndex);
+
+    setCurrentSegmentIndex(currentViewableItemIndex);
+  }, []);
+
+  useEffect(() => {
+    console.log('currentIndex', currentIndex);
+  }, [currentIndex]);
+
+  const renderItemImage = ({item}: {item: {id: string; image: any}}) => {
+    return (
+      <View style={styles.imageContainer}>
+        <Image source={item.image} resizeMode="cover" style={{width, height}} />
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.listContainer}>
+      <FlatList
+        ref={listRef}
+        data={images}
+        renderItem={renderItemImage}
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        overScrollMode="never"
+        bounces={false}
+        keyExtractor={keyExtractor}
+        nestedScrollEnabled
+        getItemLayout={getItemLayout}
+        onViewableItemsChanged={handleViewableItemsChanged}
+        initialNumToRender={1}
+        windowSize={windowSize}
+        maxToRenderPerBatch={1}
+      />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  listContainer: {
+    flex: 1,
+    backgroundColor: 'black',
   },
 });
 
