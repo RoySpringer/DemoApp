e24456125e596611ae25a209b4c0593852b0f964
Roy Springer <rspringer@slip-app.nl>
2024-05-16 09:16:49 +0200

    chore: added v0.74.1

--- ios/DemoAppLatestTests/DemoAppLatestTests.m
+++ ios/DemoAppLatestTests/DemoAppLatestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DemoAppTests : XCTestCase
+@interface DemoAppLatestTests : XCTestCase
 
 @end
 
-@implementation DemoAppTests
+@implementation DemoAppLatestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
